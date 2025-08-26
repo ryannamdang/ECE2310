5ca7aaad4cc34f91743483465678ac458e0386bd
ryannamdang <ryandang1794@gmail.com>
2025-08-26 16:13:04 -0700

    Created Hello.cpp

---
+++ Lab 1/Hello.cpp
@@ -0,0 +1,13 @@
+/*** 
+ * Coding Assignment 1: Setup
+***/
+
+#include <iostream>
+#include <cstdlib> 
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
